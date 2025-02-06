2e2b58114d4fe72eaf48a68e2727d16115b94cff
roshan ANchan <roshan.anchan@dataplatr.com>
2025-02-06 18:29:59 +0000

    COMMIT

--- models/dwh/my_first_dbt_model.sql
+++ models/dwh/my_first_dbt_model.sql
@@ -9,9 +9,11 @@
 {{ config(materialized='table') }}
 
 with source_data as (
-    select 1 as id
+    select 2 as id
     union all
     select null as id
+    union all
+    select 1 as id
 )
 
 select *
