bf2ba886fc30ca51f3372769e4c32bcf18d2a8e1
roshan ANchan <roshan.anchan@dataplatr.com>
2025-02-11 05:10:42 +0000

    commit

---
+++ models/mart/test2_model.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+with target_data as (
+    select 5 as id
+    union all
+    select 1 as id
+    union all
+    select 2 as id
+)
+
+select *
+from target_data
